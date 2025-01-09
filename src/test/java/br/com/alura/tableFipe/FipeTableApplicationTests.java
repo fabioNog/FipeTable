c0ba3779f5e5357a7a217a7d9af1ea4d049ae75a
fabionc <fabionc@unicamp.br>
2025-01-09 13:10:22 -0300

    Tranning table fipe

---
+++ src/test/java/br/com/alura/tableFipe/FipeTableApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.alura.tableFipe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FipeTableApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
